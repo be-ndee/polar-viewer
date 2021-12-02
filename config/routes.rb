f1a0a7da38401f830b2cba4e25df36306e709762
Andreas Bissinger <be-ndee@users.noreply.github.com>
2021-12-02 23:27:00 +0100

    add navigation bar to layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root "calendar#index"
-  get "/calendar", to: "calendar#index"
+
+  get "/calendar", to: "calendar#index", as: "calendar"
 
   get "/training", to: "training#list_dates", as: "training_dates"
   get "/training/:date", to: "training#list_times", as: "training_times"
