8b2472b8ac7538947bc95ef51115b9e0b9e2be90
Andreas Bissinger <be-ndee@users.noreply.github.com>
2022-03-18 23:16:52 +0100

    list last 7 days on dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,27 @@
 class DashboardController < ApplicationController
   def index
+    a_week_ago = Date.today - 7
+
     @last_workout = Workout.order("date DESC").limit(1).first()
+    workouts_last_week = Workout.where("DATE(date) >= ?", a_week_ago)
+
+    @days_last_week = []
+    date = a_week_ago
+    while date < Date.today do
+      day = {
+        date: date,
+        workouts: []
+      }
+
+      for workout in workouts_last_week
+        if workout.start.at_beginning_of_day == date.at_beginning_of_day
+          day[:workouts].append(workout)
+        end
+      end
+
+      @days_last_week.append(day)
+
+      date = date + 1
+    end
   end
 end
