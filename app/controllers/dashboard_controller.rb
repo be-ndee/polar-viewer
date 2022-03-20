ddbc17ef14e01e6894b8cb4c0de375dee2b644e0
Andreas Bissinger <be-ndee@users.noreply.github.com>
2022-03-20 17:10:53 +0100

    show last workouts with links

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,27 +1,27 @@
 class DashboardController < ApplicationController
   def index
-    a_week_ago = Date.today - 7
+    a_week_ago = Date.today - 6
 
     @last_workout = Workout.order("date DESC").limit(1).first()
     workouts_last_week = Workout.where("DATE(date) >= ?", a_week_ago)
 
     @days_last_week = []
-    date = a_week_ago
-    while date < Date.today do
+    date = Date.today
+    while date >= a_week_ago do
       day = {
         date: date,
         workouts: []
       }
 
       for workout in workouts_last_week
-        if workout.start.at_beginning_of_day == date.at_beginning_of_day
+        if workout.session.start.at_beginning_of_day == date.at_beginning_of_day
           day[:workouts].append(workout)
         end
       end
 
       @days_last_week.append(day)
 
-      date = date + 1
+      date = date - 1
     end
   end
 end
